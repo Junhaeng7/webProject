51fb1a02b8d5e416cb617fb7ea9cb1f63cb9cdac
Junhaeng <jlee3571@gatech.edu>
2022-11-30 17:19:08 -0500

    dddd

--- counter-app/src/components/Items.js
+++ counter-app/src/components/Items.js
@@ -3,22 +3,21 @@ import { useNavigate } from 'react-router-dom'
 import "./Items.css"
 
 const Items= ({id,name,price}) => {
-    const navigate = useNavigate();
-
-    const env = process.env;
-    env.PUBLIC_URL = env.PUBLIC_URL || ""
-
-    
+  const navigate = useNavigate();
+  
+  const env = process.env;
+  env.PUBLIC_URL = env.PUBLIC_URL || ""
+       
   return (
     <div className='Items'>
-        <div onClick={()=>navigate(`/shop/${id}`)}>
-            <img style={{ width: 250, height: 250 }} src = {process.env.PUBLIC_URL + `images/item${id}.jpeg`} alt="itemImage"/>
-        </div>
-        <div onClick={()=>navigate(`/shop/${id}`)}>
-            <div className='name'>{name}</div>
-            <div className='price'>${price}</div>
-        </div>
-        <button>Add to cart</button>
+      <div onClick={()=>navigate(`/shop/${id}`)}>
+        <img style={{ width: 250, height: 250 }} src = {process.env.PUBLIC_URL + `images/item${id}.jpeg`} alt="itemImage"/>
+      </div>
+      <div onClick={()=>navigate(`/shop/${id}`)}>
+          <div className='name'>{name}</div>
+          <div className='price'>${price}</div>
+      </div>
+      <button onClick={()=>navigate("/cart")}>Add to cart</button>
     </div>
   )
 }
