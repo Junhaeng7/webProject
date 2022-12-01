ad9432b25b1c110484ae86a043b0769784dd6256
Junhaeng <jlee3571@gatech.edu>
2022-12-01 02:37:12 -0500

    깃헙어렵네

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -58,55 +58,7 @@ function App() {
       price: 45,
       type: "shoes"
     },
-
   ]
-
-  const cartList = [
-    {
-      id:1,
-      name: "Jeans",
-      price: 120,
-      type: "bottom"
-    },
-    {
-      id:2,
-      name: "Jacket",
-      price: 99,
-      type: "top"
-    },
-    {
-      id:3,
-      name: "macbook",
-      price: 1599,
-      type: "other"
-    },
-    {
-      id:4,
-      name: "shoes",
-      price: 100,
-      type: "shoes"
-    },
-    {
-      id:5,
-      name: "T-shirt",
-      price: 10,
-      type: "top"
-    },{
-      id:6,
-      name: "shorts",
-      price: 50,
-      type: "bottom"
-    },
-    {
-      id:7,
-      name: "crocs",
-      price: 45,
-      type: "shoes"
-    }
-  ]
-
-  
-
 
   return (
     <>
@@ -116,7 +68,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/shop" element={<Shopping itemList = {itemList}/>} />
           <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/cart" element={<Cart cartList = {cartList} />} />
+          <Route path="/cart" element={<Cart/>} />
         </Routes>
       </Router>
     </>
